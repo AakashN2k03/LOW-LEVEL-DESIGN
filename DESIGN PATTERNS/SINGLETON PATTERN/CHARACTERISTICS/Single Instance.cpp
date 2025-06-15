3b14c77677238abc7ab1ef740748703e947d6262
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-06-16 00:01:38 +0530

    Create Single Instance.cpp

---
+++ DESIGN PATTERNS/SINGLETON PATTERN/CHARACTERISTICS/Single Instance.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Logger {
+private:
+    static Logger* instance;
+
+    Logger() {
+        cout << "Logger instance created" << endl;
+    }
+
+public:
+    static Logger* getInstance() {
+        if (instance == nullptr) {
+            instance = new Logger();  // ✅ Fix here
+        }
+        return instance;
+    }
+
+    void display() {
+        cout << "LOG initiated" << endl;
+    }
+};
+
+Logger* Logger::instance = nullptr;
+
+int main() {
+    Logger* logger1 = Logger::getInstance();
+    logger1->display();
+
+    Logger* logger2 = Logger::getInstance();
+    logger2->display();
+
+    if (logger1 == logger2) {
+        cout << "true";
+    } else {
+        cout << "false";
+    }
+
+    return 0;
+}
